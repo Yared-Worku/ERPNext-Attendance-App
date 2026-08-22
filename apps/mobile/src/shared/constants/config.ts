a6d756f80b531518fd9fadd0ff31f3508060a183
https://github.com/Yared-Worku <yaredow8343@gmail.com>
2026-08-22 15:17:24 +0300

    environment updated

--- apps/mobile/src/shared/constants/config.ts
+++ apps/mobile/src/shared/constants/config.ts
@@ -2,6 +2,8 @@ export const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL || 'http://10.5
 export const API_TIMEOUT_MS = Number(process.env.EXPO_PUBLIC_API_TIMEOUT_MS) || 10000;
 
 export const API_ENDPOINTS = {
-  RECORD_ATTENDANCE: `${API_BASE_URL}/api/method/attendance.api.record_log`,
   LOGIN: `${API_BASE_URL}/api/method/login`,
+  EMPLOYEE_RESOURCE: `${API_BASE_URL}/api/resource/Employee`,
+  RECORD_ATTENDANCE: `${API_BASE_URL}/api/method/hrms.hr.doctype.employee_checkin.employee_checkin.add_log_based_on_employee_field`,
+  ATTENDANCE_CHECKIN_RESOURCE: `${API_BASE_URL}/api/resource/Employee Checkin`,
 };
