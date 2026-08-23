8565afa84b3938c5f0ca76640993a82c3b4a4837
https://github.com/Yared-Worku <yaredow8343@gmail.com>
2026-08-24 00:21:09 +0300

    types added

---
+++ src/navigation/types.ts
@@ -0,0 +1,12 @@
+
+// The screens available when the user is NOT logged in
+export type AuthStackParamList = {
+  Login: undefined;
+};
+
+// The screens available when the user IS logged in
+export type MainStackParamList = {
+  Home: undefined;
+  Checkin: undefined;
+  History: undefined;
+};
