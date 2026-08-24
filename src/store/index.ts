d16aa82605a58379fea9d016353236b83fdba656
https://github.com/Yared-Worku <yaredow8343@gmail.com>
2026-08-24 09:27:59 +0300

    store index added

---
+++ src/store/index.ts
@@ -0,0 +1,16 @@
+
+import { create } from 'zustand';
+
+interface AuthState {
+  isAuthenticated: boolean;
+  user: string | null;
+  login: (username: string) => void;
+  logout: () => void;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  isAuthenticated: false,
+  user: null,
+  login: (username) => set({ isAuthenticated: true, user: username }),
+  logout: () => set({ isAuthenticated: false, user: null }),
+}));
