a417542fca1cbe871d8722b119d34886088d17cc
https://github.com/Yared-Worku <yaredow8343@gmail.com>
2026-09-01 15:27:18 +0300

    login screen

--- src/features/auth/screens/LoginScreen.tsx
+++ src/features/auth/screens/LoginScreen.tsx
@@ -4,7 +4,7 @@ import { AuthHeader } from '../components/AuthHeader';
 import { LoginForm } from '../components/LoginForm';
 
 export const LoginScreen = () => (
-  // Added dark:bg-slate-900 to the SafeAreaView
+
   <SafeAreaView className="flex-1 bg-gray-50 dark:bg-slate-900">
     <View className="flex-1 justify-center px-6">
       <AuthHeader />
